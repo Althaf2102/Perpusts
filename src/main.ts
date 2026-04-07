f157ff092b6aeddbd0effef7d02119c8ed34b871
Althaf2102 <fakhriza_faraby_33rpl@student.smktelkom-mlg.sch.id>
2026-04-07 19:33:08 +0700

    chore: enable prisma generate with database url

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, INestApplication } from '@nestjs/common';
+
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
-app.useGlobalPipes(new ValidationPipe());
-const config = new DocumentBuilder()
-.setTitle('Library API')
-.setDescription('Backend API Sistem Perpustakaan')
-.setVersion('1.0')
-.addBearerAuth()
-.build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-await app.listen(3003);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Library API')
+    .setDescription('Backend API Sistem Perpustakaan')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app as any, config);
+  SwaggerModule.setup('api', app as any, document);
+
+  await app.listen(3003);
+
 }
 bootstrap();
